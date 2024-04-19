1b52115475ff140a2f92d2437a14910b37f881bc
GioMa <giovanni.mayorga@hotmail.com>
2024-04-19 10:52:28 -0500

    Postprueba TableHtml

--- rickandmortyappgioma/src/components/SessionTable.js
+++ rickandmortyappgioma/src/components/SessionTable.js
@@ -2,7 +2,90 @@ import React from 'react';
 
 function Table() {
   return (
-    <div></div>
+            <div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <section class='container-fluid'>
+                            <div class='justify-content-center' style='margin-top: 100px;'>
+                                <h1>Usuarios Registrados</h1>
+
+                                <div class='table-responsive'>
+                                    <p>
+                                        <a asp-action="Insert" asp-controller="Table" class="btn btn-light">Añadir un nuevo usuario</a>
+                                    </p>
+                                    <table id="Nombre_Tabla" class='table table-striped'>
+                                        <thead>
+                                            <tr>
+                                                <th>
+                                                    IDENTIFICACIÓN
+                                                </th>
+                                                <th>
+                                                    NOMBRE
+                                                </th>
+                                                <th>
+                                                    DIRECCIÓN
+                                                </th>
+                                                <th>
+                                                    TELÉFONO
+                                                </th>
+                                                <th>
+                                                    USUARIO
+                                                </th>
+                                                <th>
+                                                    PASSWORD
+                                                </th>
+                                                <th>
+                                                    ROL
+                                                </th>
+                                                <th>
+                                                    ACTUALIZAR
+                                                </th>
+                                                <th>
+                                                    ELIMINAR
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <tr>
+                                                <td>
+                                                    
+                                                </td>
+                                                <td>
+                                                    
+                                                </td>
+                                                <td>
+                                                    
+                                                </td>
+                                                <td>
+                                                    
+                                                </td>
+                                                <td>
+                                                    
+                                                </td>
+                                                <td>
+                                                    
+                                                </td>
+                                                <td>
+                                                    
+                                                </td>
+                                                <td>
+                                                    
+                                                </td>
+                                                <td>
+                                                    
+                                                </td>
+                                            </tr>
+                                            
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </section>
+
+                    </div>
+                </div>
+            </div>
   );
 }
 
